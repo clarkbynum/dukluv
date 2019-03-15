642bc2711a1f191968d9a53e5f551bed0099e4da
TheBrokenRail <17478432+TheBrokenRail@users.noreply.github.com>
2019-03-15 17:18:21 -0400

    Update duktape.cmake

--- duktape.cmake
+++ duktape.cmake
@@ -2,9 +2,15 @@ set(DUKTAPEDIR ${CMAKE_CURRENT_LIST_DIR}/lib/duktape)
 
 include_directories(
   ${DUKTAPEDIR}/src
+  ${DUKTAPEDIR}/extras/module-duktape
+  ${DUKTAPEDIR}/extras/console
 )
 
-add_library(duktape STATIC ${DUKTAPEDIR}/src/duktape.c)
+add_library(duktape STATIC
+  ${DUKTAPEDIR}/src/duktape.c
+  ${DUKTAPEDIR}/extras/module-duktape/duk_module_duktape.c
+  ${DUKTAPEDIR}/extras/console/duk_console.c
+)
 
 if("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
   target_link_libraries(duktape
